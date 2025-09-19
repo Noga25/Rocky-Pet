fdc478e16e6c2bfa5c16aabbd1fb4b8c86f4816e
Noga25 <57655759+Noga25@users.noreply.github.com>
2025-09-19 23:04:01 +0300

    Add bubbleorfoam

--- Rocky/Assets/Scriptes/BathScriptes/SoapScript.cs
+++ Rocky/Assets/Scriptes/BathScriptes/SoapScript.cs
@@ -1,20 +1,36 @@
+using System.Collections;
+using Unity.Android.Gradle;
 using UnityEngine;
 
 public class SoapScript : MonoBehaviour
 {
+    [SerializeField] private GameObject BubbleOrFoam;
+
     private Camera MainCamera { get; set; }
     private Vector3 TouchPosition { get; set; }
     private bool IsDragging { get; set; }
 
     private float Posx { get; set; }
     private float Posy { get; set; }
+    private float Posz { get; set; }
+
+    private float BubblePosx { get; set; }
+    private float BubblePosy { get; set; }
+    private float BubblePosz { get; set; }
 
     void Start()
     {
         MainCamera = Camera.main;
 
+        //Check the begging point of the soap
         Posx = transform.position.x;
         Posy = transform.position.y;
+        Posz = transform.position.z;
+
+        //Check the begging point of the bubble
+        BubblePosx = BubbleOrFoam.transform.position.x;
+        BubblePosy = BubbleOrFoam.transform.position.y;
+        BubblePosz = BubbleOrFoam.transform.position.z;
     }
 
     void Update()
@@ -22,6 +38,7 @@ public class SoapScript : MonoBehaviour
         DruggingObject();
     }
 
+    // Respensiable on the soap and bubble location and movment and the interaction
     private void DruggingObject()
     {
         // Check if there is at least one touch on screen
@@ -34,7 +51,7 @@ public class SoapScript : MonoBehaviour
 
             if (touch.phase == TouchPhase.Began)
             {
-                // Optional: check if the finger touches the soap
+                // check if the finger touches the soap
                 Collider2D hit = Physics2D.OverlapPoint(TouchPosition);
                 if (hit != null && hit.gameObject == gameObject)
                 {
@@ -45,13 +62,19 @@ public class SoapScript : MonoBehaviour
             {
                 // Move soap with finger
                 transform.position = new Vector3(TouchPosition.x, TouchPosition.y, transform.position.z);
+
+                // show the bubble and make them go up
+                BubbleOrFoam.SetActive(true);
             }
             else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
             {
                 // Stop dragging
                 IsDragging = false;
+                transform.position = new Vector3(Posx, Posy, Posz);
 
-                transform.position = new Vector3(Posx, Posy, 0);
+                //Stop bubbles
+                BubbleOrFoam.SetActive(false);
+                BubbleOrFoam.transform.position = new Vector3( BubblePosx, BubblePosy, BubblePosz);
             }
         }
     }
